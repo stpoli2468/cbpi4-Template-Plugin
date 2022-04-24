5753a5fac022602fda72843c4136ce4ee8cfb919
stpoli2468 <75291672+stpoli2468@users.noreply.github.com>
2022-04-23 22:51:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name='{{ name }}',
+setup(name='cbpi4-Template-Plugin',
       version='0.0.1',
-      description='CraftBeerPi Plugin',
+      description='cbpi4-Template-Plugin',
       author='',
       author_email='',
-      url='',
+      url='https://github.com/stpoli2468/cbpi4-Template-Plugin',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
-      '{{ name }}': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['{{ name }}'],
+      'cbpi4-Template-Plugin': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-Template-Plugin'],
      )
